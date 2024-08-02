be3e070094346ced972910205a07e0b68a9d220a
Anas Baaj | soft-park GmbH <aba@soft-park.de>
2024-08-02 12:14:45 +0200

    ci/cd

--- ebooks/vite.config.ts
+++ ebooks/vite.config.ts
@@ -2,8 +2,12 @@
 
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import checker from 'vite-plugin-checker';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    checker({ typescript: true }),
+    react()
+  ],
 })
